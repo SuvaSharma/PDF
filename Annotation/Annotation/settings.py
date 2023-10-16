bc9a0a6a172a221dcbb65f1999bf39112e2445ff
SuvaSharma <sharmasuva1@gmail.com>
2023-10-16 13:57:53 +0545

    new stuff and other

--- Annotation/Annotation/settings.py
+++ Annotation/Annotation/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Annotation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'F:\Final_Annotation\Annotation\PDFAPP\Templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
