ba2bae58f7b1ae36275ebea3a42d76319abccd3e
SuvaSharma <sharmasuva1@gmail.com>
2023-10-16 13:37:03 +0545

    new stuff

--- Annotation/Annotation/settings.py
+++ Annotation/Annotation/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'PDFAPP',
+    'PDFAPP.apps.PdfappConfig',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +132,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
